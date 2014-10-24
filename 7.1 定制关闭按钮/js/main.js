0ce2e135d16153ef06d2c6dd074f9dbd5146756a
zhangyan <623089500@qq.com>
2014-10-24 10:05:23 +0800

    定制关闭按钮

---
+++ 7.1 定制关闭按钮/js/main.js
@@ -0,0 +1,28 @@
+require.config({
+	paths:{
+		jquery:'jquery-2.1.1.min'
+	}
+});
+
+require(['jquery','window'],function($,w){
+	$('.btn').click(function(){
+		new w.Window().alert({
+			title:'提示',
+			content:'Welcome!!!',
+			handler:function(){
+				alert('you click the button');
+			},
+			width:300,
+			height:150,
+			y:250,
+			x:600,
+			hasCloseBtn:true,
+			handler4AlertBtn:function(){
+				alert('你点击了确定按钮');
+			},
+			handler4CloseBtn:function(){
+				alert('你点击了关闭按钮');
+			}
+		});
+	});
+});
---
+++ 7.1 定制关闭按钮/js/window.js
@@ -0,0 +1,53 @@
+define(['jquery'],function($){
+	function Window(){
+		this.cfg = {
+			width:500,
+			height:300,
+			title:'系统消息',
+			content:"",
+			hasCloseBtn:false,
+			handler4AlertBtn:null,
+			handler4CloseBtn:null
+		};
+	}
+
+	Window.prototype = {
+		alert:function(cfg){
+			var CFG = $.extend(this.cfg,cfg),
+			    boudingBox = $(
+			    	'<div class="window_boundingBox">'+
+			    		'<div class="window_header">'+CFG.title+'</div>'+
+			    		'<div class="window_body">'+CFG.content+'</div>'+
+			    		'<div class="window_footer"><input type="button" class="window_alertBtn" value="确定"></div>'+
+			    	'</div>'
+			    ),
+			    btn = boudingBox.find('.window_alertBtn');
+			boudingBox.appendTo('body');
+			btn.click(function(){
+				CFG.handler4AlertBtn && CFG.handler4AlertBtn(); //弹出窗口的确定按钮处理函数
+				boudingBox.remove();
+			});
+			$.extend(this.cfg,cfg);
+			boudingBox.css({
+				width:CFG.width+'px',
+				height:CFG.height+'px',
+				left:(CFG.x||(window.innerWidth-CFG.width)/2)+'px',
+				top:(CFG.y||(window.innerHeight-CFG.height)/2)+'px'
+			});
+			if(CFG.hasCloseBtn){
+				var closeBtn = $('<span class="window_closeBtn">X</span>');
+				closeBtn.appendTo(boudingBox);
+				closeBtn.click(function(){
+					CFG.handler4CloseBtn&&CFG.handler4CloseBtn(); //弹出窗口的关闭按钮处理函数
+					boudingBox.remove();
+				});
+			}
+		},
+		confirm:function(){},
+		prompt:function(){}
+	}
+
+	return{
+		Window : Window
+	}
+})
